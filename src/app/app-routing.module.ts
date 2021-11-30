9c126ae482c022176461d2c39aee65e04edec60a
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2021-11-30 16:39:25 +0530

    sindhu changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"",redirectTo:"CompanyProfile",pathMatch:"full"},
+  {
+    path:"CompanyProfile", component: CompanyProfileComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

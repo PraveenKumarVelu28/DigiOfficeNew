dbb8dcdf860ae14144744568a99fb5ce7565a0df
Rupali Pawar <naveen.amazeinc@gmail.com>
2021-11-30 18:45:04 +0530

    ssss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from 'src/Pages/header/header.component';
+import { HelpComponent } from './help/help.component';
 
 const routes: Routes = [
   { path: 'Header', component: HeaderComponent },
-
+  {
+    path: "Help", component: HelpComponent
+  },
 ];
 
 @NgModule({

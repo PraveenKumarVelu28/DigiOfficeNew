b30dc1144e09e6ce80591f9a7f14e4461a8ed370
Anusha <anushavalmiki.amaze@gmail.com>
2021-11-30 18:35:52 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from 'src/Pages/header/header.component';
+import { CostcenterComponent } from './costcenter/costcenter.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PayrollComponent } from './payroll/payroll.component';
 
 const routes: Routes = [
   { path: 'Header', component: HeaderComponent },
+  { path: 'Payroll', component: PayrollComponent },
+  { path: 'Employee', component: EmployeeComponent },
+  {path:'Costcenter',component:CostcenterComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../Pages/header/header.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { CostcenterComponent } from './costcenter/costcenter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EmployeeComponent,
+    CostcenterComponent
   ],
   imports: [
     BrowserModule,

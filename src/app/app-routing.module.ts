680dde63e2ca1ad89bc80e8e2fa5c122b7acd21c
Anusha <anushavalmiki.amaze@gmail.com>
2021-12-01 10:54:18 +0530

    commit code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from 'src/Pages/header/header.component';
 import { CostcenterComponent } from './costcenter/costcenter.component';
+import { DepartmentsComponent } from './departments/departments.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { GeneralLedgerComponent } from './general-ledger/general-ledger.component';
 import { PayrollComponent } from './payroll/payroll.component';
 
 const routes: Routes = [
   { path: 'Header', component: HeaderComponent },
   { path: 'Payroll', component: PayrollComponent },
   { path: 'Employee', component: EmployeeComponent },
-  {path:'Costcenter',component:CostcenterComponent},
+  {path:'Costcenter',component: CostcenterComponent},
+  {path:'GeneralLedger',component: GeneralLedgerComponent},
+  {path:'Departments',component: DepartmentsComponent},
+
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../Pages/header/header.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { CostcenterComponent } from './costcenter/costcenter.component';
+import { GeneralLedgerComponent } from './general-ledger/general-ledger.component';
+import { DepartmentsComponent } from './departments/departments.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     EmployeeComponent,
-    CostcenterComponent
+    CostcenterComponent,
+    GeneralLedgerComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule,
